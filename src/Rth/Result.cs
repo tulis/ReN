8d6744010163991a677ec944bf636da1fc04c2f2
tulis <tsuryati@assetic.com>
2019-09-12 13:41:46 +1000

    Create Result class and Status enum

---
+++ src/Rth/Result.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Rth
+{
+    public class Result<TInput, TOutput, TMessage>
+    {
+        public Result(TInput input
+            , TOutput output
+            , IEnumerable<TMessage> messages
+            , Status status)
+        {
+            this.Input = input;
+            this.Output = output;
+            this.Messages = messages;
+            this.Status = status;
+        }
+
+
+        public TInput Input {get;}
+        public TOutput Output {get;}
+        public IEnumerable<TMessage> Messages {get;}
+
+
+        public Status Status {get;}
+        public bool IsError => this.Status == Status.Error;
+        public bool IsSuccess => this.Status == Status.Success;
+        public bool IsWarning => this.Status == Status.Warning;
+    }
+}
---
+++ src/Rth/Status.cs
@@ -0,0 +1,6 @@
+public enum Status
+{
+    Error
+    , Success
+    , Warning
+}
