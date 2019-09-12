bbf7895c68be2f75beb7df9e8cbf8dafc3095a9d
tulis <tsuryati@assetic.com>
2019-09-12 13:53:09 +1000

    Add Result extension for creating Success Result

---
+++ src/Rth/Result.ToSuccess.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rth
+{
+    public static class ResultToSuccess
+    {
+        public static Result<TInput, TOutput, TMessage>
+            ToSuccess<TInput, TOutput, TMessage>(
+                this TOutput output
+                , TInput input)
+            {
+                return new Result<TInput, TOutput, TMessage>(
+                    input
+                    , output
+                    , Enumerable.Empty<TMessage>()
+                    , Status.Success);
+            }
+
+        public static Result<TInput, TOutput, TMessage>
+            ToSuccess<TInput, TOutput, TMessage>(
+                this TOutput output
+                , TInput input
+                , TMessage message)
+            {
+                return new Result<TInput, TOutput, TMessage>(
+                    input
+                    , output
+                    , new[]{message}
+                    , Status.Success);
+            }
+
+        public static Result<TInput, TOutput, TMessage>
+            ToSuccess<TInput, TOutput, TMessage>(
+                this TOutput output
+                , TInput input
+                , IEnumerable<TMessage> messages)
+            {
+                return new Result<TInput, TOutput, TMessage>(
+                    input
+                    , output
+                    , messages
+                    , Status.Success);
+            }
+
+        public static Result<TInput, TOutput, TMessage>
+            ToSuccess<TInput, TOutput, TMessage>(
+                this TOutput output
+                , TInput input
+                , params TMessage[] messages)
+            {
+                return new Result<TInput, TOutput, TMessage>(
+                    input
+                    , output
+                    , messages
+                    , Status.Success);
+            }
+    }
+}
