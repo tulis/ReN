a3095d6b7ecc63ffa6174cbc865e8d18a8d14e97
tulis <tanri.suryati@outlook.com>
2020-09-20 16:45:17 +1000

    Add ResultThenTests.PreviousResultsNull test case

--- tests/Rth.Tests/ResultThenTests.cs
+++ tests/Rth.Tests/ResultThenTests.cs
@@ -1,6 +1,8 @@
 ﻿using AutoFixture.Xunit2;
+using FluentAssertions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -8,12 +10,20 @@ namespace Rth.Tests
 {
     public class ResultThenTests
     {
-        //[Theory]
-        //[InlineAutoData]
-        //public void MyTheory()
-        //{
+        [Fact]
+        public void PreviousResultsNull()
+        {
+            var previousResults = default(IEnumerable<IEnumerable<IResult<decimal, int, string>>>);
 
-        //    throw new NotImplementedException();
-        //}
+            Action then = () => previousResults
+                .Then<decimal, int, long, string>(previousOutputs => previousOutputs
+                        .Select(previousOutput => new[] { default(long).ToSuccess<int, long, string>(previousOutput) })
+                    , onNotFoundMessage: input => String.Empty)
+                .ToList();
+
+            then.Should()
+                .ThrowExactly<ArgumentNullException>()
+                .WithMessage("Value cannot be null. (Parameter 'previousResults')");
+        }
     }
 }
