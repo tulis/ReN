2c0dcc6a12b7d787301404378c5f7e382bd19762
tulis <tanri.suryati@outlook.com>
2020-12-31 15:56:34 +1100

    Revert back changing from `.DependsOn` to `.After` for `BumpVersion` Nuke Target
    Please refer to commit 9e6c181bcaf79b5e52b92e94a2ac04c87ac62b8c

--- build/Build.cs
+++ build/Build.cs
@@ -112,7 +112,7 @@ class Build : NukeBuild
         });
 
     Target BumpVersion => _ => _
-        .After(this.InstallGitVerTag, this.GitDescribeLatestTag)
+        .DependsOn(this.InstallGitVerTag, this.GitDescribeLatestTag)
         .Requires(() => GitVerTagExtension.IsBumpVersionValid(this.BUMP_VERSION))
         .Requires(() => GitTasks.GitHasCleanWorkingCopy())
         .Requires(() => !String.IsNullOrWhiteSpace(this.GitLatestTag))
